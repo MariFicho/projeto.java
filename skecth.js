567f97771a76f63f72f7effa350145b5e1daba97
MariFicho <0000112538833xsp@al.educacao.sp.gov.br>
2024-08-06 20:23:03 -0300

    adicionando arqivo js e css

---
+++ skecth.js
@@ -0,0 +1,14 @@
+//variáveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 22;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
